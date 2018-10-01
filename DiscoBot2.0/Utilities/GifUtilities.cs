1d391254fd7fe72e6b7ec5b5c3c0837e53252ec5
Josh Kerxhalli <jkerxhalli@litera.com>
2018-10-01 14:32:52 -0500

    makes sure asked for file exists

--- DiscoBot2.0/Utilities/GifUtilities.cs
+++ DiscoBot2.0/Utilities/GifUtilities.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.IO;
 
 namespace Discobot.Utilities
 {
     class GifUtilities
     {
+        public static string DefaultFace = "lampreyme.png";
         public static string DoFaceReplace(string originalGif, string face)
         {
             GifProcessing.GifProcessing processor = new GifProcessing.GifProcessing("ImageManipulation");
             string dlLocation = processor.DownloadGif(originalGif);
+            if (!File.Exists(processor.FacesPath + face))
+            {
+                face = DefaultFace;
+            }
             var newGif = processor.GifFaceSwap(face, dlLocation);
             return newGif;
         }
